735e0d170648bbd33a6de53242a13c38edfa2382
Abdullah Yıldırım <abd.yldrm543@gmail.com>
2022-10-27 11:58:29 +0300

    test

--- blog/account/tests.py
+++ blog/account/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import reverse
 
-# Create your tests here.
+# doğru veriler ile kayıt işlemi yap
+# şifre invalid olabilir
+# kullanıcı adı kayıtlı olabilir
+# üye girişi yaptıysak o sayfa gözükmemeli
+# token ile giriş işlemi yapıldığında 403 hatası
+
+class UserRegistirationTestCase(APITestCase):
+  url = reverse("account : register")
+  
+  def test_user_registiration(self):
+    """
+    doğru veriler ile kayıt işlemi yap
+    
+    """
+    data = {
+      "username" : "user",
+      "password" : "Abd*0589"
+    }
+    
+    response = self.client.post(self.url, data)
+    self.assertEqual(200, response.status_code)
+    
--- blog/post/models.py
+++ blog/post/models.py
@@ -15,6 +15,9 @@ class Post (models.Model):
   image = models.ImageField(upload_to='post')
   modified_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='modified_by')
   
+  class Meta:
+    ordering = ["-id"]
+    
   def get_slug(self):
     slug = slugify(self.title.replace("ı","i"))
     unique = slug
